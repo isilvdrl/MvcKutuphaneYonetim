7ef9074af3c7a211de4bc4388af721709cd36e5d
isilvdrl <isilvardarli.iuc@gmail.com>
2025-05-14 10:24:54 +0300

    Proje dosyası ekle.

---
+++ MvcKutuphaneYonetim/Controllers/KategoriController.cs
@@ -0,0 +1,39 @@
+﻿using MvcKutuphaneYonetim.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcKutuphaneYonetim.Controllers
+{
+    public class KategoriController : Controller
+    {
+        DBKutuphaneYonetimEntities db = new DBKutuphaneYonetimEntities();
+
+        // GET: Kategori
+        //[HttpGet]
+        public ActionResult Index()
+        {
+            var kategoriler = db.TableKategori.ToList();
+            return View(kategoriler);
+        }
+        [HttpGet]
+        public ActionResult KategoriEkle()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult KategoriEkle(TableKategori p) { 
+            db.TableKategori.Add(p);
+            db.SaveChanges();
+            return View();
+        }
+        public ActionResult KategoriSil(int id) { 
+            var kategori = db.TableKategori.Find(id);
+            db.TableKategori.Remove(kategori);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
